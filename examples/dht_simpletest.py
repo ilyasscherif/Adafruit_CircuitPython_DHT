f64f9f220c956e78162e8b6514014ce5b7930ec3
ilyasscherif <77302247+ilyasscherif@users.noreply.github.com>
2021-01-11 22:30:06 +0100

    Update dht_simpletest.py

--- examples/dht_simpletest.py
+++ examples/dht_simpletest.py
@@ -1,34 +1,72 @@
 import time
+import requests
+import math
+import random
 import board
 import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D18)
+dhtDevice = adafruit_dht.DHT11(board.D25)
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+TOKEN = "BBFF-6x8aJfDgszounyH8fqvcCw81xsSZQ5"  # Put your TOKEN here
+DEVICE_LABEL = "iot11"  # Put your device label here
+VARIABLE_LABEL_1 = "temperature"  # Put your first variable label here
+VARIABLE_LABEL_2 = "humidity"  # Put your second variable label here
+VARIABLE_LABEL_3 = "position"  # Put your second variable label here
 
-while True:
+def build_payload(variable_1, variable_2):
+    # Creates two random values for sending dat
     try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
+        value_1 = dhtDevice.temperature
+        value_2 = dhtDevice.humidity
+        payload = {variable_1: value_1,
+               variable_2: value_2,
+               }
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
+        return payload
+    except:
+        return None
 
-    time.sleep(2.0)
+    
+
+def post_request(payload):
+    # Creates the headers for the HTTP requests
+    url = "http://industrial.api.ubidots.com"
+    url = "{}/api/v1.6/devices/{}".format(url, DEVICE_LABEL)
+    headers = {"X-Auth-Token": TOKEN, "Content-Type": "application/json"}
+
+    # Makes the HTTP requests
+    status = 400
+    attempts = 0
+    while status >= 400 and attempts <= 5:
+        req = requests.post(url=url, headers=headers, json=payload)
+        status = req.status_code
+        attempts += 1
+        time.sleep(1)
+
+    # Processes results
+    print(req.status_code, req.json())
+    if status >= 400:
+        print("[ERROR] Could not send data after 5 attempts, please check \
+            your token credentials and internet connection")
+        return False
+
+    print("[INFO] request made properly, your device is updated")
+    return True
+
+
+def main():
+    payload = build_payload(
+        VARIABLE_LABEL_1, VARIABLE_LABEL_2)
+
+    print("[INFO] Attemping to send data")
+    if payload != None:
+    
+        post_request(payload)
+        
+    print("[INFO] finished")
+
+
+if __name__ == '__main__':
+    while (True):
+        main()
+        time.sleep(1)
